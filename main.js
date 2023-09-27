fb5dd6e7851dcc753cbf0af4cab9b76878b7c6c6
chandanaavirneni <146082946+chandanaavirneni@users.noreply.github.com>
2023-09-27 16:24:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function varExampleSample(){
+   a="family"; 
+    if(true){
+       var a="name";
+        alert(a);
+    a="chandana";
+        alert(a);
+        a="gopi";
+        alert(a);
+    }
+}
+function letExample(){
+    
+    b="group";
+    if (true){
+        let b="kids";
+        alert(b);
+        b="yuktha";
+        alert(b);
+        b="yashu";
+        alert(b);
+    }
+    
+}
+function constExample(){
+    let x="family";
+alert(x);
+    
+        
+}
